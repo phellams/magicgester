dd50ac36fe308fffd2192b477cc87222d9bfb7b6
sgkens <sgkens@gmail.com>
2025-01-05 12:59:52 +1100

    🎉 init: create magicgester repository

---
+++ magicgester.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 0.1.0.0
+
+.GUID 7f310bbf-d638-407a-ae67-5645fa21e0e7
+
+.AUTHOR sgkens / sgkens
+
+.COMPANYNAME sgkens
+
+.COPYRIGHT 2025 sgkens. All rights reserved.
+
+.TAGS automation
+
+.LICENSEURI https://choosealicense.com/licenses/mit
+
+.PROJECTURI https://gitlab.kenscloud.online/phellams/magicgester.git
+
+.ICONURI https://gitlab.kenscloud.online/phellams/magicgester/-/blob/main/logo.svg
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS G:\devspace\projects\powershell\_repos\gapira/magicgester-.ps1,G:\devspace\projects\powershell\_repos\gapira/magicgester-.ps1
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+https://gitlab.kenscloud.online/phellams/magicgester/-/blob/main/Releases.md
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ MagicGester is a PowerShell module that provides a number of cmdlets for exporting SVG files to various formats (PNG, ICO, JPG, etc.) using specified ranges, and returning information about the exported files, magicgester requires ImageMagick and Inkscape to be installed on the system. 
+
+#> 
+Param()
+
+
